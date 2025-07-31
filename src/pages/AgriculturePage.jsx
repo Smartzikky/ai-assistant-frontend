1dc480d25b906b242eb5a80d1bd1050dfb724e64
Smartzikky <smartisaac@gmail.com>
2025-07-31 12:23:58 +0100

    Update AgriculturePage.jsx

--- src/pages/AgriculturePage.jsx
+++ src/pages/AgriculturePage.jsx
@@ -59,7 +59,7 @@ export default function AgriculturePage() {
             type="submit"
             className="w-full py-2 px-4 rounded bg-orange-500 hover:bg-orange-600 text-white transition"
           >
-            Ask AI
+            Get Tips
           </button>
         </form>
 
